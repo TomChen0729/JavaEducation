f20bdf77152f52748befc1949ff73d17ea8773e4
TomChen0729 <tom98075@gmail.com>
2024-05-17 14:25:43 +0800

    整理國家與遊戲controller

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -23,64 +23,16 @@ class CountryController extends Controller
 
 
     // 導向等級選取，帶使用者資料，檢查玩家遊戲進度
-    public function index()
+    public function index(int $country_id)
     {
         
         //初始化玩家紀錄(檢查玩家的country_id 和 pass_familiarity_id)
         $this->gameService->initUserRecord();
 
+        $Current_User_id = auth()->user()->id;
+        $current_country = $Current_User_id;
         
         return view('level');
     }
 
-    //過去連連看畫面, 回傳資料
-    public function GoMatchGame(Request $request, int $userID)
-    {
-        // 
-        try{
-            $method = $request->method();
-            if ($method == 'GET'){
-                $currentUser = auth()->user()->id;
-                // getUserRecord($currentUser);
-
-            }
-            // return view('game.match', [""]);
-            return response()->json(['success'=> "True", 'currentUser' => $currentUser]);
-        }
-        catch(ValidationException $e){
-            return false;
-        }
-        catch(Exception $e){
-            return response()->json(['error' => 'An error occurred'], 500);
-        }
-        
-        
-        
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-    public function show(string $id)
-    {
-        //
-    }
-
-    public function edit(string $id)
-    {
-        //
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
-    }
 }
