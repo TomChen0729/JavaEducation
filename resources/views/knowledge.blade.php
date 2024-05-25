18914de90b535e1efe828f2b16e5f785bf961013
panz803 <s1411031011@nutc.edu.tw>
2024-05-25 22:16:13 +0800

    知識卡內容更新

--- resources/views/knowledge.blade.php
+++ resources/views/knowledge.blade.php
@@ -1,56 +1,104 @@
-<style>
-    *{
-        background: url('/images/background/backgroundnoroad2.svg') no-repeat center center fixed;
-        background-size: cover;
-        color: #333333;
-    }
-</style>
+@extends('layouts.application')
 
-<x-guest-layout>
-    <x-authentication-card>
+@section('title', '知識卡')
 
-        <x-validation-errors class="mb-4" />
+@section('head', '綠野仙蹤')
 
-        @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ $value }}
-            </div>
-        @endsession
+@section('style')
+    <style>
+        *{
+            font-family: 'Poppins', sans-serif;
+            box-sizing: border-box;
+            outline: none;
+            border: none;
+            text-decoration: none;
+            text-transform: capitalize;
+            transition: .2s linear;
+        }
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        .container{
+            margin-top: 100px;
+            padding: 15px 9%;
+            padding-bottom: 100px;
+        }
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+        .container .box-container{
+            /* 網格結構，組織子元素 */
+            display: grid;
+            /* 定義網格結構: 重複一組列定義(自動調整列數量，定義每列的最小和最大寬度) */
+            grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+            /* 設置網格項之間的間距為 15 像素 */
+            gap: 15px;
+        }
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" style="border: 2px solid #5b5b5b;" />
-            </div>
+        .container .box-container .box{
+            box-shadow: 0 5px 10px rgba(0, 0, 0, .2);
+            border-radius: 5px;
+            background: #fff;
+            background: linear-gradient(45deg, #fad0c4, #ffd1ff);
+            text-align: center;
+            padding: 30px 20px;
+        }
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" style="border: 2px solid #5b5b5b;" />
-                    <span class="ms-2 text-sm text-gray-600" style="color: #333333;">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+        .container .box-container .box h3{
+            color: #444;
+            font-size: 22px;
+            padding: 10px 0;
+        }
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"  style="margin-right: 20px; color: #333333;" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+        .container .box-container .box p{
+            font-size: 15px;
+            line-height: 1.8;
+        }
+
+        .container .box-container .box .btn{
+            margin-top: 10px;
+            display: inline-block;
+            background-color: #ffb6c1;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 17px;
+            font-weight: bolder;
+        }
+
+        .container .box-container .box .btn:hover{
+            letter-spacing: 1px;
+            background-color: #ea9999;
+        }
+
+        .container .box-container .box:hover{
+            box-shadow: 0 10px 15px rgba(0,0,0,.3);
+            transform: scale(1.03);
+        }
+
+        @media (max-width: 768px) {
+            .container{
+                padding: 20px;
+            }
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="box-container">
+            @foreach($knowledgecard as $knowledgecards)
+                <!-- 資料型態 -->
+                @if ($knowledgecards -> country_id == 1)
+                    <div class="box">
+                        <h3>{{ $knowledgecards -> name }}</h3>
+                        <p>{{ $knowledgecards -> content }}</p>
+                    </div>
                 @endif
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" style="color: #333333;" href="{{ route('register') }}">
-                        {{ __('Go to Register Page!') }}
-                </a>
+            @endforeach
+        </div>
+    </div>
+@endsection
 
-                <x-button class="ms-4" style="background: #fce5cd; color: #333333; border: 2px solid #5b5b5b;">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+@section('script')
+    <script>
+        
+    </script>
+@endsection
