102de6081cdceb9578d3685f44990811a736f85d
Tomchen729 <s1411031032@nutc.edu.tw>
2024-06-03 21:22:20 +0800

    對答案api未完成

--- app/Models/QuestionStatus.php
+++ app/Models/QuestionStatus.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class QuestionStatus extends Model
 {
     use HasFactory;
+    protected $table = 'question_status';
     protected $guarded = [];
-
-    public function userrecord(){
-        return $this->belongsTo(UserRecord::class, 'user_id', 'question_id');
+    
+    public function users(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function questions(){
+        return $this->belongsTo(Question::class, 'question_id');
     }
 }
