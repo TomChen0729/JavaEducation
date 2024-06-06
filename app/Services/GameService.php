f324f0a97ee3e1de1226070dea56cfd2f58d1112
TomChen0729 <tom98075@gmail.com>
2024-06-07 02:29:41 +0800

    撰寫更新當前使用者紀錄service

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -5,6 +5,8 @@ use App\Models\User;
 use GuzzleHttp\Psr7\Request;
 use App\Models\Answer;
 use App\Models\Country;
+use App\Models\Question;
+use App\Models\UserRecord;
 
 class GameService{
     public function getUserRecord(){
@@ -14,7 +16,7 @@ class GameService{
     public function initUserRecord(){
         //檢查玩家進度，如果國家id等於0和等級id等於0，就都給1進去。
         $Current_User_Country = auth()->user()->country_id;
-        $Current_User_Country_Level = auth()->user()->pass_familiarity;
+        $Current_User_Country_Level = auth()->user()->levels;
         $Current_User_id = auth()->user()->id;
         if ($Current_User_Country == null  && $Current_User_Country_Level == null){
             $user = User::find($Current_User_id);
@@ -24,7 +26,37 @@ class GameService{
             ]);
         }
     }
-    public function updateUserRecord(int $user_id){
+
+    // 傳入當前國家id, 當前levels
+    public function updateUserRecord(int $country_id, int $levels){
+        $current_user_id = auth()->user()->id;
+        // 查詢該國家該等級應該有的不重複玩法形成集合
+        $gametype_set = Question::select('game_type')
+        ->where('country_id', $country_id)
+        ->where('levels', $levels)->distinct()->get();
+        // 調取目前玩家有的當前國家當前等級不重複玩法集合(未完成)
+        $user_gametype_set = UserRecord::all();
+        // 如果兩者相等的話，等級加一
+        if(count($gametype_set) == count($user_gametype_set)){
+            $current_user_levels = auth()->user()->levels;
+            $current_user_levels ++;
+            $current_user = User::find($current_user_id);
+            $current_user->update([
+                'levels' => $current_user_levels
+            ]);
+            // 查詢當前玩家在該國家的等級是否是最高，如果是，levels變成1，country_id ++
+            $current_country = 0;
+            if(auth()->user()->levels){
+                $current_user_country = auth()->user()->country_id;
+                $current_user_country ++;
+                $current_user = User::find($current_user_id);
+                $current_user->update([
+                    'country_id' => $current_user_country
+                ]);
+            }
+        }
+    }
+    public function giveUserCards(int $user_id){
         //將要改變的資料從前端接收
         //將資料存入變數
         //使用ORM update CurrentUserRecord
@@ -46,7 +78,7 @@ class GameService{
     // 關卡需要使用者具備哪些知識卡
     public function CheckCurrentGameKGrequired(int $Country_id){
         // 國家->難度->關卡->知識卡要求
-        $CurrentCountry = Country::find($Country_id); // 當前國家
+        $CurrentCountry = Country::where('country_id', $Country_id); // 當前國家
         $CurrentCountry->pass_familiarities;
         
         return ;
