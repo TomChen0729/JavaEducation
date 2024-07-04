21df9c525d24a30340362c87f97ec5b38bc7b2d4
panz803 <s1411031011@nutc.edu.tw>
2024-07-04 21:06:32 +0800

    配色更新

--- resources/views/game/match.blade.php
+++ resources/views/game/match.blade.php
@@ -38,7 +38,7 @@
             position: fixed;
             top: 0px;
             left: 0px;
-            width: 100vw;
+            width: 100%;
             height: 100%;
             background: rgba(0, 0, 0, 0.7);
             z-index: 1;
@@ -50,35 +50,31 @@
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%) scale(0);
-            background: #fff;
-            width: 950px;
+            background: #4D613C;
+            border-radius: 50px;
+            width: 50%;
             z-index: 1;
             padding: 20px;
             box-sizing: border-box;
         }
 
         .first .pop {
-            color: #333333;
+            color: #F8F2ED;
             margin: 30px;
             padding: 30px;
             border-radius: 50px;
-            border: 5px solid #333333;
+            border: 5px solid #F6B654;
         }
 
         .first .pop h1 {
             text-align: center;
-            font-size: 26px;
+            font-size: 30px;
             font-weight: bolder;
-            margin-bottom: 5px;
+            margin-bottom: 30px;
         }
 
         .first .pop p {
             font-size: 16px;
-        }
-
-        .first .pop p.cen {
-            text-align: center;
-            margin-bottom: 20px;
         }
 
         .first .pop hr {
@@ -92,8 +88,8 @@
             top: 20px;
             width: 30px;
             height: 30px;
-            background-color: #222;
-            color: #fff;
+            background-color: #D6C9B6;
+            color: #5B1718;
             font-size: 25px;
             font-weight: 600;
             line-height: 30px;
@@ -109,7 +105,7 @@
             transition: all 300ms ease-in-out;
             transform: translate(-50%, -50%) scale(1);
         }
-        
+
         .popup .overlay {
             position: fixed;
             top: 0px;
@@ -126,9 +122,10 @@
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%) scale(0);
-            background: #fff;
-            width: 450px;
-            height: 220px;
+            background: #91806E;
+            border-radius: 50px;
+            width: 40%;
+            height: 50%;
             z-index: 2;
             text-align: center;
             padding: 20px;
@@ -136,20 +133,38 @@
         }
 
         .popup .pop {
+            color: #F8F2ED;
+            height: 80%;
             margin: 30px;
             padding: 30px 0;
             border-radius: 50px;
-            border: 5px solid #333333;
+            border: 5px solid #A9C2BC;
         }
 
         .popup .pop h1 {
             font-size: 20px;
             font-weight: bolder;
-            margin-bottom: 5px;
+            margin-bottom: 30px;
         }
 
         .popup .pop p {
             font-size: 16px;
+        }
+
+        .popup .pop a {
+            color: #F8F2ED;
+            font-size: 20px;
+            font-weight: bold;
+            background-color: #A9C2BC;
+            border-radius: 10px;
+            padding: 10px;
+            margin: 10px;
+            margin-top: 30px;
+        }
+
+        .popup .pop a:hover {
+            color: #F8F2ED;
+            background-color: #91806E;
         }
 
         .popup .close-btn {
@@ -159,8 +174,8 @@
             top: 20px;
             width: 30px;
             height: 30px;
-            background-color: #222;
-            color: #fff;
+            background-color: #A9C2BC;
+            color: #F8F2ED;
             font-size: 25px;
             font-weight: 600;
             line-height: 30px;
@@ -177,6 +192,87 @@
             transform: translate(-50%, -50%) scale(1);
         }
 
+        
+        .end .overlay {
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.7);
+            z-index: 1;
+            display: none;
+        }
+
+        .end .content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) scale(0);
+            background: #452E34;
+            border-radius: 30px;
+            width: 40%;
+            z-index: 2;
+            text-align: center;
+            padding: 20px;
+            box-sizing: border-box;
+        }
+
+        .end .pop {
+            color: #E7E9DC;
+            margin: 30px;
+            padding: 30px 0;
+            border-radius: 50px;
+            border: 5px solid #876E51;
+            height: 50%;
+        }
+
+        .end .pop h1 {
+            font-size: 30px;
+            font-weight: bolder;
+            margin-bottom: 30px;
+        }
+
+        .end .pop a {
+            font-size: 20px;
+            font-weight: bold;
+            background-color: #876E51;
+            border-radius: 10px;
+            padding: 10px;
+            margin: 10px;
+            margin-top: 30px;
+        }
+
+        .end .pop a:hover {
+            color: #876E51;
+            background-color: #F7E9DC;
+        }
+
+        .end .close-btn {
+            cursor: pointer;
+            position: absolute;
+            right: 20px;
+            top: 20px;
+            width: 30px;
+            height: 30px;
+            background-color: #876E51;
+            color: #F7E9DC;
+            font-size: 25px;
+            font-weight: 600;
+            line-height: 30px;
+            text-align: center;
+            border-radius: 50%;
+        }
+
+        .end.active .overlay {
+            display: block;
+        }
+
+        .end.active .content {
+            transition: all 300ms ease-in-out;
+            transform: translate(-50%, -50%) scale(1);
+        }
+
         header {
             position: fixed;
             width: 100%;
@@ -187,7 +283,7 @@
             align-items: center;
             justify-content: space-between;
             padding: 28px 2%;
-            background: rgba(72, 170, 193, 0.8);
+            background: rgba(156,197,196, 0.8);
             /* 透明背景 */
             transition: all 0.50s ease;
         }
@@ -230,7 +326,7 @@
 
         .breadcrumbs__link__active {
             text-decoration: none;
-            color: #009578;
+            color: #A34343;
             font-weight: bold;
         }
 
@@ -373,12 +469,13 @@
 
         .selected {
             /* 選中項目有邊框顯示 */
-            outline: 3px solid #0000ff;
+            outline: 3px solid #394165;
         }
 
         .matched {
             /* 配對成功後的樣式 */
-            background-color: #00ff00;
+            background-color: #394165;
+            color: #E38931;
             pointer-events: none;
             /* 禁用已配對元素的點擊事件 */
         }
@@ -452,47 +549,47 @@
 </head>
 
 <body>
+    <!-- 彈窗 -->
+    <!-- 遊戲說明 -->
     <div class="first active" id="popup">
         <div class="overlay"></div>
         <div class="content">
             <div class="close-btn" onclick="togglePopup1()">&times;</div>
             <div class="pop">
                 <h1>遊戲說明</h1>
-                <p class="cen">歡迎來到《綠野仙蹤》遊戲，在這個遊戲中，你將會經歷兩個大關卡：學習區和闖關區。</p>
-                <p><strong>學習區</strong><br>
-                    在學習區，你將會遇到四種不同的關卡類型：是非、選擇、重組和配對。</p>
-                <p><strong>闖關區</strong><br>
-                    在完成學習區的所有關卡後，你將進入闖關區。</p>
-                <hr>
-                <p><strong>獎勵</strong><br>
-                    成功通過學習區後，你將獲得一包知識卡，這將幫助你更好地理解和運用相關知識，來決戰闖關區。</p>
-                <p>準備好開始你的冒險了嗎？快來挑戰這個充滿知識與樂趣的遊戲，提升你的編程技能吧！</p>
+                <p><strong>配對題</strong><br>
+                    判斷左方紅色框格中的敘述，配對右方黃框相對應的選項
+                    需要先選擇紅色框格，再選黃框對應的選項</p>
             </div>
         </div>
     </div>
 
+    <!-- 知識卡選擇 -->
     <div class="popup" id="popup-1">
         <div class="overlay"></div>
         <div class="content">
-            <div class="close-btn" onclick="togglePopup()">&times;</div>
+            <div class="close-btn" onclick="togglePopup2()">&times;</div>
             <div class="pop">
-                <a href="#" onclick="togglePopups()">知識卡</a>
+                <a href="#" onclick="togglePopup3()">知識卡</a>
             </div>
         </div>
     </div>
 
-    @foreach ( $questions_cards as $item)
-    <div class="popup" id="popup-2">
+    <!-- 知識卡資訊 -->
+    
+
+    <!-- 答題正確 -->
+    <div class="end" id="popup-3">
         <div class="overlay"></div>
         <div class="content">
-            <div class="close-btn" onclick="togglePopups()">&times;</div>
+            <div class="close-btn" onclick="togglePopup4()">&times;</div>
             <div class="pop">
-                <h1>{{ $item -> name }}</h1>
-                <p>{{ $item -> content }}</p>
+                <h1>答案正確</h1>
+                <a href="#" onclick="history.go(-1)">遊戲種類</a>
+                <a href="#">繼續答題</a>
             </div>
         </div>
     </div>
-    @endforeach
 
     <header class="header">
         <ul class="breadcrumbs">
@@ -589,6 +686,10 @@
 
         function togglePopup3() {
             document.getElementById("popup-2").classList.toggle("active");
+        }
+
+        function togglePopup4() {
+            document.getElementById("popup-3").classList.toggle("active");
         }
 
         // 遊戲
