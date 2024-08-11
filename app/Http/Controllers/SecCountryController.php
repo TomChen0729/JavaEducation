0a736dd274a42820b48dc7c10f630400a696f1a6
TomChen0729 <tom98075@gmail.com>
2024-08-11 15:47:39 +0800

    secCountryController gameService注入

--- app/Http/Controllers/SecCountryController.php
+++ app/Http/Controllers/SecCountryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\GameService;
 use Illuminate\Http\Request;
 
 class SecCountryController extends Controller
 {
     //
+    protected $gameService;
+
+    public function __construct(GameService $gameService){
+        $this->gameService = $gameService;
+    }
 }
