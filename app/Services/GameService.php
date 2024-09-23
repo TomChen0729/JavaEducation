a43ac3e226348a88debb759a728ecbb207a02d85
TomChen0729 <tom98075@gmail.com>
2024-09-23 10:33:47 +0800

    使用者進入南國後，等級歸0，sec_records表加入使用者答案欄位

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -37,113 +37,121 @@ class GameService
     public function updateUserRecord()
     {
         $current_user = auth()->user();
-        // 查詢玩家當前最高國家最高等級應該有的不重複玩法形成集合(if -> country_id=1 & levels=1，找1, 1中會出現的)
-        $gametype_set = Question::select('gametype')
-            ->where('country_id', $current_user->country_id)
-            ->where('levels', $current_user->levels)->distinct()->get();
-        // 調取目前玩家有的最高國家最高等級玩過且正確不重複玩法集合(if -> country_id=1 & levels=1，找1, 1中玩過且正確的)
-        // questions->users_records->distinct gametype欄位
-        $user_gametype_set = Question::join('user_records', 'user_records.question_id', '=', 'questions.id')
-            ->where('country_id', $current_user->country_id)
-            ->where('levels', $current_user->levels)
-            ->where('user_id', $current_user->id)
-            ->where('status', 1)
-            ->distinct()->pluck('questions.gametype');
+        $currentUserCountry = $current_user->country_id;
+        switch ($currentUserCountry) {
+            case 1:
+                // 查詢玩家當前最高國家最高等級應該有的不重複玩法形成集合(if -> country_id=1 & levels=1，找1, 1中會出現的)
+                $gametype_set = Question::select('gametype')
+                    ->where('country_id', $current_user->country_id)
+                    ->where('levels', $current_user->levels)->distinct()->get();
+                // 調取目前玩家有的最高國家最高等級玩過且正確不重複玩法集合(if -> country_id=1 & levels=1，找1, 1中玩過且正確的)
+                // questions->users_records->distinct gametype欄位
+                $user_gametype_set = Question::join('user_records', 'user_records.question_id', '=', 'questions.id')
+                    ->where('country_id', $current_user->country_id)
+                    ->where('levels', $current_user->levels)
+                    ->where('user_id', $current_user->id)
+                    ->where('status', 1)
+                    ->distinct()->pluck('questions.gametype');
 
-        $countryMaxLV = CardType::where('country_id', $current_user->country_id)->max('levels');
-        // 查看他自己的遊玩進度是不是最大的
-        // 如果不是的話，才進行等級晉升動作
-        if (!User::where('id', $current_user->id)->where('country_id', $current_user->country_id)->where('levels', $countryMaxLV)->exists()) {
-            // 不是最大的話，檢查他遊玩進度的最大等級的所有學習區類型是否玩過且有正確紀錄
-            // 如果兩者長度相等的話，等級加一
-            if (count($gametype_set) == count($user_gametype_set)) {
-                if (!User::where('id', $current_user->id)->where('country_id', $current_user->country_id)->where('levels', $current_user->levels + 1)->exists()) {
-                    // 抓當前國家id & levels，找出他的cardtype有哪些(之後可能會有一個levels對很多個type的問題，到時候有需要的話改迴圈)
-                    $all_cardtypes_in_current_lv = CardType::where('country_id', $current_user->country_id)->where('levels', $current_user->levels)->pluck('id');
-                    // 利用找到的card_type_id，去資料庫拉相關的所有知識卡
-                    $all_cards_in_current_lv = KnowledgeCard::whereIn('card_type_id', $all_cardtypes_in_current_lv)->get();
-                    // 新增一個當前使用者的UserKnowledgeCard物件
-                    $current_user_card = new UserKnowledgeCard();
-                    if ((count($all_cards_in_current_lv) != 0)) {
-                        foreach ($all_cards_in_current_lv as $item) {
-                            $current_user_card->create([
-                                'user_id' => $current_user->id,
-                                'knowledge_card_id' => $item->id,
-                                'watchtime' => '00:00:00'
+                $countryMaxLV = CardType::where('country_id', $current_user->country_id)->max('levels');
+                // 查看他自己的遊玩進度是不是最大的
+                // 如果不是的話，才進行等級晉升動作
+                if (!User::where('id', $current_user->id)->where('country_id', $current_user->country_id)->where('levels', $countryMaxLV)->exists()) {
+                    // 不是最大的話，檢查他遊玩進度的最大等級的所有學習區類型是否玩過且有正確紀錄
+                    // 如果兩者長度相等的話，等級加一
+                    if (count($gametype_set) == count($user_gametype_set)) {
+                        if (!User::where('id', $current_user->id)->where('country_id', $current_user->country_id)->where('levels', $current_user->levels + 1)->exists()) {
+                            // 抓當前國家id & levels，找出他的cardtype有哪些(之後可能會有一個levels對很多個type的問題，到時候有需要的話改迴圈)
+                            $all_cardtypes_in_current_lv = CardType::where('country_id', $current_user->country_id)->where('levels', $current_user->levels)->pluck('id');
+                            // 利用找到的card_type_id，去資料庫拉相關的所有知識卡
+                            $all_cards_in_current_lv = KnowledgeCard::whereIn('card_type_id', $all_cardtypes_in_current_lv)->get();
+                            // 新增一個當前使用者的UserKnowledgeCard物件
+                            $current_user_card = new UserKnowledgeCard();
+                            if ((count($all_cards_in_current_lv) != 0)) {
+                                foreach ($all_cards_in_current_lv as $item) {
+                                    $current_user_card->create([
+                                        'user_id' => $current_user->id,
+                                        'knowledge_card_id' => $item->id,
+                                        'watchtime' => '00:00:00'
+                                    ]);
+                                }
+                            }
+                            $current_user_levels = auth()->user()->levels;
+                            $current_user_levels++;
+                            $current_user = User::find($current_user->id);
+                            $current_user->update([
+                                'levels' => $current_user_levels
                             ]);
                         }
                     }
-                    $current_user_levels = auth()->user()->levels;
-                    $current_user_levels++;
-                    $current_user = User::find($current_user->id);
-                    $current_user->update([
-                        'levels' => $current_user_levels
-                    ]);
-                }
-            }
-        } else {
-            // 這邊是他已經在他最高的國家中已經是最高等級
-            // 再去檢查他該國家dubug類型是否有正確了一題
-            // 抓玩過且正確的所有debug_id
-            $debugID = DebugRecord::where('user_id', auth()->user()->id)->where('status', 1)->pluck('debug_id')->toArray();
-            // 帶$debugID查詢debugs表題目，計算筆數是否>=1
-            if (Debug::whereIn('id', $debugID)->where('country_id', $current_user->country_id)->get()->count() >= 1) {
-                // 升級國家
-                // 等級回到一
-                $current_user_country = auth()->user()->country_id;
-                $current_user_country++;
-                $current_user = User::find($current_user->id);
-                $current_user->update([
-                    'country_id' => $current_user_country,
-                    'levels' => 1
-                ]);
-            } else {
-                // 找出最高等級的所有 gametype
-                $gametype_set_MAX = Question::select('gametype')
-                    ->where('country_id', $current_user->country_id)
-                    ->where('levels', $countryMaxLV)
-                    ->distinct()
-                    ->get();
-                // 找出玩家已經完成且正確的 gametype 集合
-                $user_gametype_set_MAX = Question::join('user_records', 'user_records.question_id', '=', 'questions.id')
-                    ->where('questions.country_id', $current_user->country_id)
-                    ->where('questions.levels', $countryMaxLV)
-                    ->where('user_records.user_id', $current_user->id)
-                    ->where('user_records.status', 1)
-                    ->distinct()
-                    ->pluck('questions.gametype');
-                // 比較兩個集合大小
-                if (count($gametype_set_MAX) == count($user_gametype_set_MAX)) {
-                    // 找出最高等級的所有cardtype
-                    $all_cardtypes_in_max_lv = CardType::where('country_id', $current_user->country_id)
-                        ->where('levels', $countryMaxLV)
-                        ->pluck('id')
-                        ->toArray();
-                    // 利用找到的card_type_id，去資料庫拉相關的所有知識卡
-                    $all_cards_in_max_lv = KnowledgeCard::whereIn('card_type_id', $all_cardtypes_in_max_lv)->get();
-                    // 找到玩家已經擁有的知識卡
-                    $owned_knowledge_card_id = UserKnowledgeCard::where('user_id', $current_user->id)
-                        ->pluck('knowledge_card_id')
-                        ->toArray();
-                    // 過濾掉玩家已經擁有的知識卡，避免重複派發
-                    $new_cards = $all_cards_in_max_lv->filter(function ($card) use ($owned_knowledge_card_id) {
-                        // 回傳多出來的
-                        return !in_array($card->id, $owned_knowledge_card_id);
-                    });
-                    // 產生需要插入的資料，以及其格式
-                    if ($new_cards->isNotEmpty()) {
-                        $new_card_data = $new_cards->map(function ($card) use ($current_user) {
-                            return [
-                                'user_id' => $current_user->id,
-                                'knowledge_card_id' => $card->id,
-                                'watchtime' => '00:00:00',
-                            ];
-                        })->toArray();
-                        // 執行插入
-                        UserKnowledgeCard::insert($new_card_data);
+                } else {
+                    // 這邊是他已經在他最高的國家中已經是最高等級
+                    // 再去檢查他該國家dubug類型是否有正確了一題
+                    // 抓玩過且正確的所有debug_id
+                    $debugID = DebugRecord::where('user_id', auth()->user()->id)->where('status', 1)->pluck('debug_id')->toArray();
+                    // 帶$debugID查詢debugs表題目，計算筆數是否>=1
+                    if (Debug::whereIn('id', $debugID)->where('country_id', $current_user->country_id)->get()->count() >= 1) {
+                        // 升級國家
+                        // 等級回到一
+                        $current_user_country = auth()->user()->country_id;
+                        $current_user_country++;
+                        $current_user = User::find($current_user->id);
+                        $current_user->update([
+                            'country_id' => $current_user_country,
+                            'levels' => 1
+                        ]);
+                    } else {
+                        // 找出最高等級的所有 gametype
+                        $gametype_set_MAX = Question::select('gametype')
+                            ->where('country_id', $current_user->country_id)
+                            ->where('levels', $countryMaxLV)
+                            ->distinct()
+                            ->get();
+                        // 找出玩家已經完成且正確的 gametype 集合
+                        $user_gametype_set_MAX = Question::join('user_records', 'user_records.question_id', '=', 'questions.id')
+                            ->where('questions.country_id', $current_user->country_id)
+                            ->where('questions.levels', $countryMaxLV)
+                            ->where('user_records.user_id', $current_user->id)
+                            ->where('user_records.status', 1)
+                            ->distinct()
+                            ->pluck('questions.gametype');
+                        // 比較兩個集合大小
+                        if (count($gametype_set_MAX) == count($user_gametype_set_MAX)) {
+                            // 找出最高等級的所有cardtype
+                            $all_cardtypes_in_max_lv = CardType::where('country_id', $current_user->country_id)
+                                ->where('levels', $countryMaxLV)
+                                ->pluck('id')
+                                ->toArray();
+                            // 利用找到的card_type_id，去資料庫拉相關的所有知識卡
+                            $all_cards_in_max_lv = KnowledgeCard::whereIn('card_type_id', $all_cardtypes_in_max_lv)->get();
+                            // 找到玩家已經擁有的知識卡
+                            $owned_knowledge_card_id = UserKnowledgeCard::where('user_id', $current_user->id)
+                                ->pluck('knowledge_card_id')
+                                ->toArray();
+                            // 過濾掉玩家已經擁有的知識卡，避免重複派發
+                            $new_cards = $all_cards_in_max_lv->filter(function ($card) use ($owned_knowledge_card_id) {
+                                // 回傳多出來的
+                                return !in_array($card->id, $owned_knowledge_card_id);
+                            });
+                            // 產生需要插入的資料，以及其格式
+                            if ($new_cards->isNotEmpty()) {
+                                $new_card_data = $new_cards->map(function ($card) use ($current_user) {
+                                    return [
+                                        'user_id' => $current_user->id,
+                                        'knowledge_card_id' => $card->id,
+                                        'watchtime' => '00:00:00',
+                                    ];
+                                })->toArray();
+                                // 執行插入
+                                UserKnowledgeCard::insert($new_card_data);
+                            }
+                        }
                     }
                 }
-            }
+                break;
+            case 2:
+                User::find(auth()->user()->id)->update(['levels' => 0]);
+            default:
         }
     }
 
---
+++ database/migrations/2024_09_23_013533_add_user_answer_to_sec_records.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sec_records', function (Blueprint $table) {
+            //
+            $table->string('user_answer', 1000)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sec_records', function (Blueprint $table) {
+            //
+            $table->dropColumn('user_answer');
+        });
+    }
+};
