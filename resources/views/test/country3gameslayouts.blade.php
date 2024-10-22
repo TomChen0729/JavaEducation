3897ad57a8f8c0d6b8cfce60de0267d91a55d710
TomChen0729 <tom98075@gmail.com>
2024-10-22 17:57:32 +0800

    國家三前端測試版

---
+++ resources/views/test/country3gameslayouts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="col1">
+            <div class="row1">
+
+            </div>
+            <div class="row2">
+
+            </div>
+        </div>
+        <div class="col2">
+            <div class="row1">
+
+            </div>
+            <div class="row2">
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
